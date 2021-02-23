ebb270b0e0b0d29cb665cc53f3ca0ccd492179e6
Bartłomiej Popiołek <popiolekbartlomiej369@gmail.com>
2021-02-23 20:48:58 +0100

    fix background

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ import Content from "./components/Content";
 function App() {
     return (
         <>
-            <div className="bg-black bg-opacity-50 fixed inset-0 backdrop-blur z-10"></div>
-            <div className="bg-image-2 bg-cover bg-center bg-no-repeat">
+            <div className="bg-image-2 bg-cover bg-center bg-no-repeat relative">
+                <div className="bg-black bg-opacity-50 absolute inset-0 backdrop-blur z-10"></div>
                 <div className="w-11/12 m-auto z-20 relative">
                     <LanguageIcons />
                     <div className="w-full m-auto flex">
